5354b059b2664114c8f529e8ef9724ae528311d7
Chaitab <146720244+Chaitab@users.noreply.github.com>
2023-10-02 16:53:39 +0530

    Update streamlit_app.py
    listing fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,9 @@ streamlit.title('🥗 Kakarkaya Karam')
 streamlit.title('🥑 Vankaya Pulusu')
 streamlit.title('🍞 Majjiga Charu')
 
-streamlit.header('🍌🥭 Build Your Own Veg Curry 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own fruit smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
