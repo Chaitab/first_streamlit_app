0a58b5b5419ec3de62aa7f7dbdb33013f324eddd
Chaitab <146720244+Chaitab@users.noreply.github.com>
2023-10-02 16:45:46 +0530

    Update streamlit_app.py
    Added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('My Restaurant new healthy diner')
 
-streamlit.header('Lunch Menu')
-streamlit.title('Fhalakurra fhappu')
-streamlit.title('Kakarkaya Karam')
-streamlit.title('Vankaya Pulusu')
-streamlit.title('Majjiga Charu')
+streamlit.header('Lunch Favourites')
+streamlit.title('🥣 Fhalakurra fhappu')
+streamlit.title('🥗 Kakarkaya Karam')
+streamlit.title('🥑 Vankaya Pulusu')
+streamlit.title('🍞 Majjiga Charu')
